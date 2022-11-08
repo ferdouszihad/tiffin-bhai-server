9f9d968cd8ebf43fa698361ac09adf7b0b7d68e2
Your Name~ <f.zihad333@gmail.com>
2022-11-08 17:20:37 +0600

    server connected with mongoDB

--- index.js
+++ index.js
@@ -2,8 +2,26 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(cors());
+
+// userName : adminDB
+// pass : fTHTcrpRTW7E1XdH
+
+const uri =
+  "mongodb+srv://adminDB:fTHTcrpRTW7E1XdH@cluster0.kxkmed0.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log("database connected");
+  client.close();
+});
 
 app.get("/", (req, res) => {
   res.send("Hello Ferdous! Dashing Handsome Guy 😎");
