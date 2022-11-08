6f7a5f54a222331f2683a1fdd2165c7223db0b10
Your Name~ <f.zihad333@gmail.com>
2022-11-08 10:51:52 +0600

    start script added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.port || 5000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World Zihad mia!");
 });
 
 app.listen(port, () => {
